01505104621c8b871d604ecfe290fc4681faf6e9
minichole <ferranlozano@protonmail.com>
2021-05-26 18:27:49 +0200

    Menu de pause
    He creado un menu de pause que te puede llevar al menu principal, salir o volver a la partida

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public static bool gameP;
+    public static bool boolseguroP;
+
+    public GameObject menuP, seguroP;
+
+    private void Start()
+    {
+        menuP.SetActive(false);
+        seguroP.SetActive(false);
+    }
+
+
+    public void SwitchPause()
+    {
+        if (gameP)
+        {
+            btnResume();
+        }
+        else
+        {
+            btnPause();
+        }
+    }
+
+
+    void btnResume()
+    {
+        menuP.SetActive(false);
+        Time.timeScale = 1;
+        gameP = false;
+    }
+
+    void btnPause()
+    {
+        menuP.SetActive(true);
+        Time.timeScale = 0;
+        gameP = true;
+    }
+
+
+    public void mPrincipal(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void panel2()
+    {
+        seguroP.SetActive(true);
+    }
+
+
+    public void salirPno()
+    {
+        seguroP.SetActive(false);
+    }
+
+    public void salirPsi()
+    {
+        Application.Quit();
+    }
+
+}
