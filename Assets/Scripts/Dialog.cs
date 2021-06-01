4099ee331c3ee954a37300a6c4b55e57a2b5c2cc
Kevin Marquez <kevinmarquez1367@gmail.com>
2021-06-01 19:46:25 +0200

    Añadido dialogos/mensajes con sus respeectivas funciones
    -Creada pantalla de dialogo
    -Creado script de los dialogos con sus implementaciones.
    -Añadido icono de aviso.
    -Creada funcion que oculta los controles mientras se ven los mensajes.

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textD;
+    
+    [TextArea (3,30)]
+    public string[] parrafos;
+
+    int index;
+
+    public float velParrafo;
+
+    public GameObject botonContinue;
+    public GameObject botonQuitar;
+    public GameObject panelDialogo;
+    public GameObject botonLeer;
+    public GameObject controles;
+    public GameObject info;
+
+    
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        botonQuitar.SetActive(false);
+        botonLeer.SetActive(false);
+        panelDialogo.SetActive(false);
+        
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (textD.text == parrafos[index])
+        {
+            botonContinue.SetActive(true);
+        }
+    }
+
+    IEnumerator TextDialogo(){
+        foreach (char letra in parrafos[index].ToCharArray())
+        {
+            textD.text += letra;
+            yield return new WaitForSeconds(velParrafo);
+
+        }
+    }
+
+
+    public void siguienteParrafo(){
+        botonContinue.SetActive(false);
+        if(index < parrafos.Length -1 ){
+            index++;
+            textD.text= "";
+            StartCoroutine(TextDialogo());
+
+        }
+        else{
+            textD.text= "";
+            botonContinue.SetActive(false);
+            botonQuitar.SetActive(true);
+        }
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            botonLeer.SetActive(true);
+        }
+        else
+        {
+            botonLeer.SetActive(false);
+        }
+    }
+
+
+    public void activarBotonLeer()
+    {
+        controles.SetActive(false);
+        panelDialogo.SetActive(true);
+        StartCoroutine(TextDialogo());
+    }
+
+    public void botonCerrar()
+    {
+        panelDialogo.SetActive(false);
+        botonLeer.SetActive(false);
+        controles.SetActive(true);
+        info.SetActive(false);
+    }
+
+}
